85ca4b2c52190d3092258efe43f5467e4abf1a13
AndyOmondi17 <andyomondi17@gmail.com>
2019-09-02 09:12:43 +0300

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /* UI logic*/
 
-$(document).ready(function(){
+$(document).ready(function(event){
   $("#od").click(function(){
     $(".form_").show();
     $("input[name=delivery]").click(function(){
@@ -23,3 +23,51 @@ $(document).ready(function(){
 });
 
 /*Business logic*/
+
+
+
+$(document).ready(function(event){
+  $(".form_").submit(function() {
+    var pizzaSize=$("#size").val();
+    var sizePrice;
+    switch (pizzaSize) {
+      case "1":
+      sizePrice=340;
+      break;
+      case "2":
+      sizePrice=500;
+      break;
+      case "3":
+      sizePrice=700;
+      break;
+      default:
+      sizePrice=0;
+    }
+    var crust=$("#crust").val();
+    var crustPrice;
+    switch (crust){
+      case "1":
+      crustPrice=100;
+      break;
+      case "2":
+      crustPrice=200;
+      break;
+      case "3":
+      crustPrice=300;
+      break;
+      default:
+      crustPrice=0;
+    }
+    var toppings=$().val();
+    var delivery=$("input[name=delivery:checked]").val();
+    var deliveryPrice;
+    if(deliver == "yes"){
+      deliveryPrice=150;
+    }else {
+      deliveryPrice=0;
+    }
+    var totalPrice=crustPrice+deliveryPrice+sizePrice;
+    alert("Your total price is"+totalPrice);
+
+  });
+});
